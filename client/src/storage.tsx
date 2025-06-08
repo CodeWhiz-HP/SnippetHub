9eeccd75345b47a821ba5b265aabdf109a501a87
CodeWhiz-HP <harshalp1106@gmail.com>
2025-06-08 20:14:59 +0530

    Changed API URLs

--- client/src/storage.tsx
+++ client/src/storage.tsx
@@ -2,34 +2,36 @@ import { Category } from "./types/category";
 import { Snippet } from "./types/snippet";
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_URL;
+
 export async function fetchSnippets(): Promise<Snippet[]> {
-  const response = await axios.get("http://localhost:5000/api/snippets");
+  const response = await axios.get(`${baseURL}/snippets`);
   return response.data;
 }
 
 export async function saveSnippet(snippet: Snippet): Promise<Snippet> {
-  const response = await axios.post("http://localhost:5000/api/snippets", snippet);
+  const response = await axios.post(`${baseURL}/snippets`, snippet);
   return response.data;
 }
 
 export async function deleteSnippet(id: string) {
-  await axios.delete(`http://localhost:5000/api/snippets/${id}`);
+  await axios.delete(`${baseURL}/snippets/${id}`);
 }
 
 export async function updateSnippet(id: string, snippet: Partial<Snippet>) {
-  const response = await axios.put(`http://localhost:5000/api/snippets/${id}`, snippet);
+  const response = await axios.put(`${baseURL}/snippets/${id}`, snippet);
   return response.data;
 }
 
 export async function fetchCategories(): Promise<Category[]> {
-  const res = await axios.get("http://localhost:5000/api/categories");
+  const res = await axios.get(`${baseURL}/categories`);
   return res.data;
 }
 
 export async function saveCategory(name: string) {
-  return axios.post("http://localhost:5000/api/categories", { name });
+  return axios.post(`${baseURL}/categories`, { name });
 }
 
 export async function deleteCategory(id: string) {
-  return axios.delete(`http://localhost:5000/api/categories/${id}`);
+  return axios.delete(`${baseURL}/categories/${id}`);
 }
