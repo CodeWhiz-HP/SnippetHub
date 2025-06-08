721ced5f93566aa501419ed53fd95e9c1514429b
CodeWhiz-HP <harshalp1106@gmail.com>
2025-06-08 21:06:01 +0530

    Updated Login Form

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -9,7 +9,7 @@ export default function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [code, setCode] = useState("");
+    const [code , setCode] =useState("");
     const navigate = useNavigate();
 
     const handleLogin = async (e: any) => {
@@ -26,18 +26,12 @@ export default function Login() {
     };
 
 
-    return (
-        <div className="flex flex-col gap-2">
-            <form onSubmit={handleLogin} className="flex flex-col gap-4 login-form w-[30%] h-[48%] p-4 m-2 mb-6 bg-white rounded-md">
-                <h2 className="w-fit text-2xl rounded-md font-montserrat mb-3 text-white bg-indigo500 p-2">Login</h2>
-                <input className="text-lg p-3 rounded-md h-10 bg-gray200" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-                <input className="text-lg p-3 rounded-md h-10 bg-gray200" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-                <button className="bg-indigo-600 text-white px-2 py-1 rounded hover:bg-indigo-700 hover:cursor-pointer w-fit mt-4">Submit</button>
-            </form>
-            <form >
-                <input className="text-lg p-3 rounded-md h-10 bg-gray200" value={code} onChange={(e) => setCode(e.target.value)} placeholder="6-digit code" />
-                <button className="bg-indigo-600 text-white px-2 py-1 rounded hover:bg-indigo-700 hover:cursor-pointer w-fit mt-4">Login</button>
-            </form>
-        </div>
+    return(
+        <form onSubmit={handleLogin} className="flex flex-col gap-4 login-form w-[30%] h-[48%] p-4 m-2 mb-6 bg-white rounded-md">
+            <h2 className="w-fit text-2xl rounded-md font-montserrat mb-3 text-white bg-indigo500 p-2">Login</h2>
+            <input className="text-lg p-3 rounded-md h-10 bg-gray200" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+            <input className="text-lg p-3 rounded-md h-10 bg-gray200" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+            <button className="bg-indigo-600 text-white px-2 py-1 rounded hover:bg-indigo-700 hover:cursor-pointer w-fit mt-8">Login</button>
+        </form>
     );
 };
