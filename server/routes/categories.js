ec48ccac289d0d0280e7f5d0f811c4624e0108d1
CodeWhiz-HP <harshalp1106@gmail.com>
2025-06-08 21:00:20 +0530

    Improvised Snippets Access.

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const Category = require("../models/Category");
 const router = express.Router();
+const AuthMiddleware = require("../routes/auth");
 
 
 
-router.get('/', async(req,res) => {
+router.get('/',AuthMiddleware, async(req,res) => {
     const categories = await Category.find();
     res.json(categories);
 });
 
-router.post('/', async(req,res) => {
+router.post('/',AuthMiddleware, async(req,res) => {
     const category = new Category(req.body);
     await category.save();
     res.status(201).json(category);
 });
 
-router.delete('/:id', async (req,res) => {
+router.delete('/:id',AuthMiddleware, async (req,res) => {
     const category = await Category.findByIdAndDelete(req.params.id);
     res.status(204).end();
 });
--- server/routes/snippets.js
+++ server/routes/snippets.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const Snippet = require("../models/Snippet");
 const router = express.Router();
+const AuthMiddleware = require("../routes/auth")
 
 
 
-router.get('/', async(req,res) => {
+router.get('/',AuthMiddleware, async(req,res) => {
     const snippets = await Snippet.find();
     res.json(snippets);
 });
 
-router.post('/', async(req,res) => {
+router.post('/',AuthMiddleware, async(req,res) => {
     const snippet = new Snippet(req.body);
     await snippet.save();
     res.status(201).json(snippet);
 });
 
-router.delete('/:id', async (req,res) => {
+router.delete('/:id',AuthMiddleware, async (req,res) => {
     const snippet = Snippet.findByIdAndDelete(req.params.id);
     res.status(204).end();
 });
 
-router.put('/:id', async (req,res) => {
+router.put('/:id',AuthMiddleware, async (req,res) => {
     const snippet = await Snippet.findByIdAndUpdate(req.params.id, req.body,{new : true});
     res.json(snippet);
 })
